6a8126e737a2aa58f289b52e26a6a568c24e4db7
Sunveloper <tmws.chinnawatr@gmail.com>
2020-10-15 10:57:10 +0700

    changes

--- src/ThemeServiceProvider.php
+++ src/ThemeServiceProvider.php
@@ -25,20 +25,16 @@ use Laradic\Themes\Assets\AssetFactory;
 class ThemeServiceProvider extends ServiceProvider
 {
     protected $providers = [
-        \Laradic\Themes\Providers\BusServiceProvider::class,
+        /* \Laradic\Themes\Providers\BusServiceProvider::class, */
         \Laradic\Themes\Providers\EventServiceProvider::class,
         \Laradic\Themes\Providers\ConsoleServiceProvider::class,
-        \Collective\Html\HtmlServiceProvider::class
+        \Collective\Html\HtmlServiceProvider::class,
     ];
-
-
     public function boot()
     {
         /** @var \Illuminate\Foundation\Application $app */
         $app = parent::boot();
-
     }
-
     /**
      * Register the service provider.
      *
@@ -48,52 +44,39 @@ class ThemeServiceProvider extends ServiceProvider
     {
         /** @var \Illuminate\Foundation\Application $app */
         $app = parent::register();
-
         $this->linkConfig();
-
         $this->registerAssets();
         $this->registerThemes();
         $this->registerViewFinder();
-
-        $app->make('events')->listen('creating: *', function (\Illuminate\Contracts\View\View $view) use ($app)
-        {
+        $app->make('events')->listen('creating: *', function (\Illuminate\Contracts\View\View $view) use ($app) {
             $app->make('themes')->boot();
         });
-
     }
-
     protected function linkConfig()
     {
         $configPath = realpath(__DIR__ . '/../resources/config/config.php');
         $this->mergeConfigFrom($configPath, 'laradic.themes');
-        $this->publishes([ $configPath => config_path('laradic.themes.php') ], 'config');
+        $this->publishes([$configPath => config_path('laradic.themes.php')], 'config');
     }
-
     protected function registerAssets()
     {
-        $this->app->singleton('assets', function (Application $app)
-        {
-            $assetFactory = new AssetFactory($app[ 'themes' ]); //Laradic\Themes\Assets\AssetFactory
+        $this->app->singleton('assets', function (Application $app) {
+            $assetFactory = new AssetFactory($app['themes']); //Laradic\Themes\Assets\AssetFactory
             $assetFactory->setAssetClass(config('laradic.themes.assetClass'));
             $assetFactory->setAssetGroupClass(config('laradic.themes.assetGroupClass'));
             $assetFactory->setCachePath(config('laradic.themes.paths.cache'));
-            foreach ( config('laradic.themes.assets.globalFilters') as $extension => $filters )
-            {
-                foreach ( $filters as $filter )
-                {
+            foreach (config('laradic.themes.assets.globalFilters') as $extension => $filters) {
+                foreach ($filters as $filter) {
                     $assetFactory->addGlobalFilter($extension, $filter);
                 }
             }
-
             return $assetFactory;
         });
         $this->app->alias('assets', 'Laradic\Themes\Contracts\AssetFactory');
     }
-
     protected function registerThemes()
     {
-        $this->app->singleton('themes', function (Application $app)
-        {
+        $this->app->singleton('themes', function (Application $app) {
             $themeFactory = new ThemeFactory($app->make('files'), $app->make('events'));
             $themeFactory->setPaths(config('laradic.themes.paths'));
             $themeFactory->setThemeClass(config('laradic.themes.themeClass'));
@@ -103,40 +86,28 @@ class ThemeServiceProvider extends ServiceProvider
         });
         $this->app->alias('themes', 'Laradic\Themes\Contracts\ThemeFactory');
     }
-
-
     protected function registerViewFinder()
     {
         /**
          * @var $oldViewFinder FileViewFinder
          */
-        $oldViewFinder = $this->app[ 'view.finder' ];
-
-        $this->app->bind('view.finder', function ($app) use ($oldViewFinder)
-        {
+        $oldViewFinder = $this->app['view.finder'];
+        $this->app->bind('view.finder', function ($app) use ($oldViewFinder) {
             $paths = array_merge(
-                $app[ 'config' ][ 'view.paths' ],
+                $app['config']['view.paths'],
                 $oldViewFinder->getPaths()
             );
-
-            $themesViewFinder = new ThemeViewFinder($app[ 'files' ], $paths, $oldViewFinder->getExtensions());
-            $themesViewFinder->setThemes($app[ 'themes' ]);
-            $app[ 'themes' ]->setFinder($themesViewFinder);
-
-            foreach ( $oldViewFinder->getPaths() as $location )
-            {
+            $themesViewFinder = new ThemeViewFinder($app['files'], $paths, $oldViewFinder->getExtensions());
+            $themesViewFinder->setThemes($app['themes']);
+            $app['themes']->setFinder($themesViewFinder);
+            foreach ($oldViewFinder->getPaths() as $location) {
                 $themesViewFinder->addLocation($location);
             }
-
-            foreach ( $oldViewFinder->getHints() as $namespace => $hints )
-            {
+            foreach ($oldViewFinder->getHints() as $namespace => $hints) {
                 $themesViewFinder->addNamespace($namespace, $hints);
             }
-
             return $themesViewFinder;
         });
-
-        $this->app[ 'view' ]->setFinder($this->app[ 'view.finder' ]);
+        $this->app['view']->setFinder($this->app['view.finder']);
     }
-
 }
